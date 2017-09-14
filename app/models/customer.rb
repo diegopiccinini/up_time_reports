ab81cacac73d8688aa3af1a53eaec6ef4c225d28
Diego H Piccinini Lagos <dlagos@bookingbug.com>
2017-09-14 17:24:10 +0100

    added acts as paranoid

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+
+  acts_as_paranoid
+
+  has_many :vpcs
+
 end
--- app/models/vpc.rb
+++ app/models/vpc.rb
@@ -1,4 +1,5 @@
 class Vpc < ApplicationRecord
+  acts_as_paranoid
   belongs_to :customer
 
   def self.checks
