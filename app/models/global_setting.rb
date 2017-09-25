e668ee992d76c059f5272488728a6948fcac3c00
Diego H Piccinini Lagos <dlagos@bookingbug.com>
2017-09-25 10:04:20 +0100

    tested global setting set and get

--- app/models/global_setting.rb
+++ app/models/global_setting.rb
@@ -1,6 +1,14 @@
 class GlobalSetting < ApplicationRecord
+
   def self.get name
     s=self.find_by name: name
     s ? JSON.parse(s.data,symbolize_names: true) : false
   end
+
+  def self.set name, data_hash
+    s=self.find_or_create_by name: name
+    s.data= data_hash.to_json
+    s.save
+  end
+
 end
--- test/models/global_setting_test.rb
+++ test/models/global_setting_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class GlobalSettingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "#set and get" do
+
+    data_hash = { id: 1, name: 'Name 1' }
+    assert GlobalSetting.set( 'test_var', data_hash)
+
+    get_hash = GlobalSetting.get 'test_var'
+    assert_equal get_hash, data_hash
+  end
 end
