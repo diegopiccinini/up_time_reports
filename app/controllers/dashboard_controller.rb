7708d0fec21c17b958d6b8597696f2bdf2aa9684
Diego H Piccinini Lagos <dlagos@bookingbug.com>
2017-10-17 10:29:30 +0100

    updated test and protect frontend app with password

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,16 @@
 class DashboardController < ApplicationController
   layout 'frontend'
+  http_basic_authenticate_with name: "bbug", password: "aszx12qw"
+
   def index
     @global_reports=GlobalReport.built.order(updated_at: :desc).all
+
     @latest_global_daily=GlobalReport.where(period: 'day').built.last
-    @daily_reports=@latest_global_daily.reports.includes(:vpc).order('vpcs.name')
+    @daily_reports=[]
+    @daily_reports=@latest_global_daily.reports.includes(:vpc).order('vpcs.name') if @latest_global_daily
+
     @latest_global_weekly=GlobalReport.where(period: 'week').built.last
-    @weekly_reports=@latest_global_weekly.reports.includes(:vpc).order('vpcs.name')
+    @weekly_reports=[]
+    @weekly_reports=@latest_global_weekly.reports.includes(:vpc).order('vpcs.name') if @latest_global_weekly
   end
 end
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get dashboard_index_url
-    assert_response :success
+    get root_url
+    assert_response :unauthorized
   end
 
 end
