c280d4cd6544fe66af9dd90ce4f106c62c464941
Diego H Piccinini Lagos <dlagos@bookingbug.com>
2017-11-03 16:20:56 +0000

    set the last report order by start date desc

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,11 +9,11 @@ class DashboardController < ApplicationController
     @monthly_global_reports=GlobalReport.built.where(period: 'month', resolution: 'day').order(start_date: :desc).limit(10).all
     @yearly_global_reports=GlobalReport.built.where(period: 'year').order(updated_at: :desc).limit(10).all
 
-    @latest_global_daily=GlobalReport.where(period: 'day').built.last
+    @latest_global_daily=GlobalReport.where(period: 'day').built.order(start_date: :desc).limit(1).first
     @daily_reports=[]
     @daily_reports=@latest_global_daily.reports.includes(:vpc).order('vpcs.name') if @latest_global_daily
 
-    @latest_global_weekly=GlobalReport.where(period: 'week').built.last
+    @latest_global_weekly=GlobalReport.where(period: 'week').built.order(start_date: :desc).limit(1).first
     @weekly_reports=[]
     @weekly_reports=@latest_global_weekly.reports.includes(:vpc).order('vpcs.name') if @latest_global_weekly
   end
